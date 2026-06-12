6cc21960f5726947000ee9bfecccb0ea842ba60d
Marcos Lucas de Souza Machado <118454658+MarcosLucasdeSouzaMachado@users.noreply.github.com>
2026-06-12 10:17:36 -0300

    Add files via upload

--- Cliente.py
+++ Cliente.py
@@ -1,23 +1,23 @@
 from socket import *
-import Servidor
 
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input('Digite uma frase minuscula:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print ('Resposta do servidor:', modifiedSentence.decode())
-clientSocket.close()
 
 # Informações básicas Cliente.
 NomeJogador = input('Digite o nome do jogador: ')
-NivelJogador = int(input('Digite o nivel do jogador: '))
+clientSocket.send(NomeJogador.encode())
+NivelJogador = input('Digite o nivel do jogador: ')
+clientSocket.send(NivelJogador.encode())
 print('jogos: ')
 print('--- Valorant, LOL, CS, FIFA, Minecraft ---')
 JogoDesejado = input('Deseja jogar qual jogo?: ')
+clientSocket.send(JogoDesejado.encode())
 
-return NomeJogador, NivelJogador, JogoDesejado
+MSG = clientSocket.recv(1024)
+print("Resposta do jogo: ", MSG.decode())
 
-# Resposta ao cliente.
+modifiedSentence = clientSocket.recv(1024)
+print ('Resposta do servidor:', modifiedSentence.decode())
+clientSocket.close()
--- Servidor.py
+++ Servidor.py
@@ -1,18 +1,54 @@
 from socket import *
-import Cliente
+
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print('O servidor esta pronto esperando mensagens')
+
+#Listas
+Valorant = []
+LOL = []
+CS = []
+FIFA = []
+Minecraft = []
+
+#preenchimento
 while True:
 	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024).decode()
-	print("mensagem recebida: ", sentence)
-	capitalizedSentence = sentence.upper()
-	print("mensagem que sera enviada: ", capitalizedSentence)
-	connectionSocket.send(capitalizedSentence.encode())
+	Nome = connectionSocket.recv(1024).decode()
+	print("Nome do Jogador: ", Nome)
+	Nivel = connectionSocket.recv(1024).decode()
+	print("Nivel do Jogador: ", Nivel)
+	Jogo = connectionSocket.recv(1024).decode()
+	print("Jogo do Jogador: ", Jogo)
+
+	#separação
+	if Jogo == 'Valorant':
+		Valorant.append(Nome)
+		MSG = 'Na lista de espera'
+		connectionSocket.send(MSG.encode())
+	elif Jogo == 'LOL':
+		LOL.append(Nome)
+		MSG = 'Na lista de espera'
+		connectionSocket.send(MSG.encode())
+	elif Jogo == 'CS':
+		CS.append(Nome)
+		MSG = 'Na lista de espera'
+		connectionSocket.send(MSG.encode())
+	elif Jogo == 'FIFA':
+		FIFA.append(Nome)
+		MSG = 'Na lista de espera'
+		connectionSocket.send(MSG.encode())
+	elif Jogo == 'Minecraft':
+		Minecraft.append(Nome)
+		MSG = 'Na lista de espera'
+		connectionSocket.send(MSG.encode())
+	else:
+		erro = 'Ocorreu um erro no servidor'
+		connectionSocket.send(erro.encode())
+
+
 	connectionSocket.close()
 
-# Informações importadas:
-Nome = Cliente.NomeJogador()
+
